087c055212110df3d4c470637971410cb844384a
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2025-07-30 00:02:33 +0530

    changes made to folder structure

---
+++ sorting/insertionSort.cpp
@@ -0,0 +1,32 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+void insertion_sort(int arr[], int n){
+    cout << "Sorting the array.." << endl;
+    for(int i = 0; i < n; i++){
+        int j = i;
+        while(j>0 && arr[j] < arr[j-1]){
+            swap(arr[j],arr[j-1]);
+            j--;
+        }
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter no. of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i <= n-1; i++){
+        cin >> arr[i];
+    }
+
+    insertion_sort(arr, n);
+
+    cout << "SORTED ARRAY: " << endl;
+    for(int i = 0; i <= n-1; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
---
+++ sorting/selectionSort.cpp
@@ -0,0 +1,33 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+void selection_sort(int arr[], int n){
+    for(int i = 0; i < n - 1; i++) {
+        int minIndex = i;
+        for(int j = i ; j < n; j++) {
+            if(arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+        swap(arr[i], arr[minIndex]);
+    }
+}
+
+int main(){
+    int n;
+    cout << "Enter no. of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i <= n-1; i++){
+        cin >> arr[i];
+    }
+
+    selection_sort(arr, n);
+
+    cout << "SORTED ARRAY: " << endl;
+    for(int i = 0; i <= n-1; i++){
+        cout << arr[i] << " ";
+    }
+
+    return 0;
+}
