7bb9368334a70d1620d952cbf228cf932dfbaeb4
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2025-08-03 11:27:29 +0530

    changes made in algorithms

--- searching/binary-search.cpp
+++ searching/binary-search.cpp
@@ -3,7 +3,15 @@ using namespace std;
 
 /*
 ALGORITHM:
-1. 
+Step 1 − Select the middle item in the array and compare it with the key value to be searched. If it is matched, return the position of the median.
+
+Step 2 − If it does not match the key value, check if the key value is either greater than or less than the median value.
+
+Step 3 − If the key is greater, perform the search in the right sub-array; but if the key is lower than the median value, perform the search in the left sub-array.
+
+Step 4 − Repeat Steps 1, 2 and 3 iteratively, until the size of sub-array becomes 1.
+
+Step 5 − If the key value does not exist in the array, then the algorithm returns an unsuccessful search.
 */
 
 int main(){
--- searching/linear-search.cpp
+++ searching/linear-search.cpp
@@ -3,10 +3,35 @@ using namespace std;
 
 /*
 ALGORITHM:
-1. 
+Step 1 − Start from the 0th index of the input array, compare the key value with the value present in the 0th index.
+
+Step 2 − If the value matches with the key, return the position at which the value was found.
+
+Step 3 − If the value does not match with the key, compare the next element in the array.
+
+Step 4 − Repeat Step 3 until there is a match found. Return the position at which the match was found.
+
+Step 5 − If it is an unsuccessful search, print that the element is not present in the array and exit the program.
 */
 
 int main(){
-    
+    int n, search;
+    cout << "Enter no. of elements: ";
+    cin >> n;
+    int arr[n];
+    for(int i = 0; i <= n-1; i++){
+        cin >> arr[i];
+    }
+    cout << "Enter element to search: ";
+    cin >> search;
+
+    // Linear / Sequential Search
+    for (int i = 0; i < n; i++){
+        if (arr[i] == search){
+                cout << "Element found at index " << i;
+                return 0;
+        }
+    }
+    cout << "Element not found.";
     return 0;
 }
