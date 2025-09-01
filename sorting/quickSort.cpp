c160e36227a3a892a82b0f3c064db7da6d7818cc
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2025-09-01 07:34:30 +0530

    updations in sorting

---
+++ sorting/quickSort.cpp
@@ -0,0 +1,21 @@
+#include<bits\stdc++.h>
+using namespace std;
+
+/* ALGORITHM: 
+-----------------------------------------------------------------------------
+1. Choose the highest index value has pivot
+2. Take two variables to point left and right of the list excluding pivot
+3. Left points to the low index
+4. Right points to the high
+5. While value at left is less than pivot move right
+6. While value at right is greater than pivot move left
+7. If both step 5 and step 6 does not match swap left and right
+8. If left ≥ right, the point where they met is new pivot
+-----------------------------------------------------------------------------
+
+*/
+
+int main(){
+    
+    return 0;
+}
--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -1,9 +1,13 @@
 #include<bits\stdc++.h>
 using namespace std;
 
-/*
-ALGORITHM:
-1. 
+/* ALGORITHM:
+1.  Find the smallest element and swap it with the first element. 
+    This way we get the smallest element at its correct position.
+
+2.  Then we find the next smallest among remaining elements and swap it with the second element.
+
+3.  We keep doing this until we get all elements moved to correct position.
 */
 
 void selection_sort(int arr[], int n){
