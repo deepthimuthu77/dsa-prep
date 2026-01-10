a9c48d103801f227412aa0c37d97bf26a74f6899
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2026-01-11 00:25:01 +0530

    arrays lc

---
+++ leetcode/move-zeros.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int index = 0;
+        for(int num:nums){
+            if(num!=0) swap(num,nums[index++]);
+        }
+        for(int i=index; i<nums.size();i++) nums[i] = 0;
+        
+    }
+};
