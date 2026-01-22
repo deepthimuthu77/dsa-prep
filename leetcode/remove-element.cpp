d4db330078b4bb64bcacb60fa0e2b11cc032d50c
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2026-01-22 23:40:56 +0530

    more lc medium

---
+++ leetcode/remove-element.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int i=0, j=nums.size();
+        while(i<j){
+            if(nums[i] == val){
+                nums[i] = nums[j-1];
+                j--;
+            } else i++;
+        }
+        return j;
+    }
+};
