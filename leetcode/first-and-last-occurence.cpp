47a2705423c7e0fb6ad3b2d95e095fc0513ed018
Deepthi Muthukumar <deepthimuthu77@gmail.com>
2026-02-10 23:23:19 +0530

    lc2

---
+++ leetcode/first-and-last-occurence.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+
+    int firstOcc(vector<int>& arr, int low, int high, int key){
+        int first = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] == key){
+                first = mid;
+                high = mid-1;
+            }else if(arr[mid]<key){
+                low=mid+1;
+            } else high = mid-1;
+        }
+        return first;
+    }
+    int lastOcc(vector<int>& arr, int low, int high, int key){
+        int last = -1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid] == key){
+                last = mid;
+                low = mid+1;
+            }else if(arr[mid]<key){
+                low=mid+1;
+            } else high = mid-1;
+        }
+        return last;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = firstOcc(nums,0,nums.size()-1,target);
+        int last = lastOcc(nums,0,nums.size()-1,target);
+        return {first,last};        
+    }
+};
